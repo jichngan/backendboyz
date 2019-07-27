60a927027a28f633d4bbfa9b86a9e07469fed1dd
jichngan <jcngan97@gmail.com>
2019-07-27 16:32:00 +0800

    270719 Commit, removed old mac dashboard

--- mac_dashboard/subprocesscall.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-child = subprocess.Popen(['python', 'plotpoisontest.py'], stdin=subprocess.PIPE)
-try:  
-  child.communicate('192.168.0.168')
-except KeyboardInterrupt:
-  child.terminate()
